30922cf6a63b84b015ebb42ebc224e84a69ef9e6
Megan Deray <Megrde93@gmail.com>
2021-11-08 15:18:42 -0500

    megan

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const ctx = canvas.getContext('2d')
 
+// THE PLATE 
+
 let plate = new Image()
 plate.src = 'images/pancake.jpeg'
 plate.onload = () => {
@@ -22,14 +24,14 @@ window.onkeydown = function (e) {
         case 'ArrowLeft':
         newPlate.x -= 50;
             break;
-
         case 'ArrowRight':
         newPlate.x += 50;
         break
     } 
 }
+//  Falling Obstacle
 
-class Roadblock{
+class badTopping{
     constructor(x,y,w,h){
     this.x = Math.random()*canvas.width,
     this.y = -55,
@@ -39,12 +41,12 @@ class Roadblock{
     }
 }
 
-let pointcounter = 0
-const roadblocks = []
+let pointcounterBadTopping = 0
+const badToppings = []
 
 setInterval(() => {
-    pointcounter+=100
-roadblocks.push(new Roadblock())
+    pointcounterBadTopping+=100
+badToppings.push(new badTopping())
 }, 2500)
 
 let int
@@ -53,27 +55,83 @@ function animate() {
     int = window.requestAnimationFrame(animate)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = 'purple'
-for (let roadblock of roadblocks){
-    ctx.fillRect(roadblock.x, roadblock.y +=(2*roadblock.speedModifier), roadblock.w, roadblock.h)
-    detectCollision(newPlate, roadblock)
+for (let badTopping of badToppings){
+    ctx.fillRect(badTopping.x, badTopping.y +=(2*badTopping.speedModifier), badTopping.w, badTopping.h)
+    detectCollision(newPlate, badTopping)
     }
-console.log(roadblocks)
+console.log(badToppings)
   
     ctx.drawImage(plate, newPlate.x, newPlate.y, newPlate.w, newPlate.h)
   
-    document.querySelector('body p').innerHTML = pointcounter
+    document.querySelector('body p').innerHTML = pointcounterBadTopping
 
   }
   
   animate()
 
-  function detectCollision(thePlate, theObstacle) {
-    if (thePlate.x < theObstacle.x + theObstacle.w &&
-        thePlate.x + thePlate.w > theObstacle.x &&
-        theCar.y < theObstacle.y + theObstacle.h &&
-        theCar.h + theCar.y > theObstacle.y) {
+// Falling pancakes 
+
+class pancake{
+    constructor(x,y,w,h){
+    this.x = Math.random()*canvas.width,
+    this.y = -55,
+    this.w = 50,
+    this.h = 50,
+    this.speedModifier = Math.random()*2
+    }
+}
+
+let pointcounterPancake = 0
+const pancakes = []
+
+setInterval(() => {
+    pointcounter+=100
+pancakes.push(new pancake())
+}, 2500)
+
+// let int
+
+// function animate() {
+//     int = window.requestAnimationFrame(animate)
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     ctx.fillStyle = 'purple'
+// for (let pancake of pancakes){
+//     ctx.fillRect(pancake.x, pancake.y +=(2*pancake.speedModifier), pancake.w, pancake.h)
+//     detectCollision(newPlate, pancake)
+//     }
+// console.log(pancakes)
+  
+//     ctx.drawImage(plate, newPlate.x, newPlate.y, newPlate.w, newPlate.h)
+  
+//     document.querySelector('body p').innerHTML = pointcounter
+
+//   }
+  
+//   animate()
+
+// Falling bonus 
+
+
+
+
+
+// Collision Logics
+
+  function detectCollision(thePlate, badTopping) {
+    if (thePlate.x < badTopping.x + badTopping.w &&
+        thePlate.x + thePlate.w > badTopping.x &&
+        thePlate.y < badTopping.y + badTopping.h &&
+        thePlate.h + thePlate.y > badTopping.y) {
         console.log('collision')
-        window.cancelAnimationFrame(int)
-        // window.location.reload()
+        // window.cancelAnimationFrame(int)
+        window.location.reload()
     }
   }
+
+//   Our pancake stack
+// let theStack = new Array [] 
+// function stackMyCakes{
+// if(something colides & is the object we want) 
+// {theStack.push(that thing)}
+
+
