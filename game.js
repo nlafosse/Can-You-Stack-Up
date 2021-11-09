57d2c0e3ebfb74fcaafeb048eb86b533de97101d
Nathalia Lafosse <lafossenathalia@gmail.com>
2021-11-09 12:39:04 -0500

    collision still

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const ctx = canvas.getContext('2d')
 
+
+
 // THE PLATE 
 let plate = new Image()
 plate.src = 'images/pancake.jpeg'
@@ -10,12 +12,28 @@ plate.onload = () => {
     ctx.drawImage(plate, 150, canvas.height - 260, 158, 250)
 }
 
+let stackedArray = []  // new array for collided objs that need to stack
+
+function addItem(){
+    itemArr.push(new Item(id++))
+  }
+
 let newPlate = {
     x: 150,
     y: canvas.height-260,
     w: 325,
     h: 250,
 } 
+
+// class newPlate {
+//     constructor(x, y, w, h){
+//     this.x =150,
+//     this.y = canvas.height-260,
+//     this.w = 325,
+//     this.h = 250
+//     // this.stackedArray = [plate]
+//     }
+// }
 
 window.onkeydown = function (e) {
     switch (e.key) {
@@ -28,14 +46,16 @@ window.onkeydown = function (e) {
     } 
 }
 
+
 //  Falling objects
 class badTopping{
-    constructor(x,y,w,h){
+    constructor(id){
     this.x = Math.random()*canvas.width,
     this.y = -55,
     this.w = 50,
     this.h = 50,
-    this.speedModifier = Math.random()*2
+    this.speedModifier = Math.random()*2,
+    this.id = id
     }
 }
 class Pancake{
@@ -59,32 +79,39 @@ class Bonus{
 
 
 // arrays of falling objects
+
+
 let pointcounterBadTopping = 0
-const badToppings = []
+const badToppingsArr = []
 
 let pointcounterBonus = 0
-const bonuses = []
+const bonusesArr = []
 
 let pointcounterPancake = 0
-const pancakes = []
+const pancakesArr = []
 
-let stackedArray = []  // new array for collided objs that need to stack
+let id = 0
+
+function addItem(){
+  badToppingsArr.push(new badTopping(id++))
+}
+
 
 
 // intervals for falling objects
 setInterval(() => {
     pointcounterBadTopping+=100
-    badToppings.push(new badTopping())
+    badToppingsArr.push(new badTopping())
     }, 2000)
 
 setInterval(() => {
     pointcounterPancake+=100
-    pancakes.push(new Pancake())
+    pancakesArr.push(new Pancake())
     }, 2500)
     
 setInterval(() => {
     pointcounterBonus+=100
-    bonuses.push(new Bonus())
+    bonusesArr.push(new Bonus())
     }, 3000)
 
 let int
@@ -94,28 +121,28 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     // draw badToppings objs
     ctx.fillStyle = 'purple'
-    for (let badTopping of badToppings){
-        ctx.fillRect(badTopping.x, badTopping.y +=(2*badTopping.speedModifier), badTopping.w, badTopping.h)
-        detectCollision(newPlate, badTopping)
+    for (let badTopping of badToppingsArr){
+        ctx.fillRect(badTopping.x, badTopping.y +=(2*badTopping.speedModifier), badTopping.w, badTopping.h, badTopping.id)
+        detectBadToppingCollision(newPlate, badTopping)
         }
     // draw pancakes objs
     ctx.fillStyle = 'yellow'
-    for (let pancake of pancakes){
+    for (let pancake of pancakesArr){
         ctx.fillRect(pancake.x, pancake.y +=(2*pancake.speedModifier), pancake.w, pancake.h)
-        detectCollision(newPlate, pancake)
+        // detectPancakeCollision(newPlate, pancake)
         }
     // draw bonus objs
     ctx.fillStyle = 'pink'
-    for (let bonus of bonuses){
+    for (let bonus of bonusesArr){
         ctx.fillRect(bonus.x, bonus.y +=(2*bonus.speedModifier), bonus.w, bonus.h)
-        detectCollision(newPlate, bonus)
+        // detectBonusCollision(newPlate, bonus)
         } 
     // plate catcher
     ctx.drawImage(plate, newPlate.x, newPlate.y, newPlate.w, newPlate.h)
-    for (let i =0; i< stackedArray.length; i++){
-        ctx.fillRect(newPlate.x, newPlate.y - stackedArray[i+1] * 50, stackedArray[i].w, stackedArray[i].h)
-        detectCollision(newPlate, stackedArray[i])
-    }
+    // for (let i =0; i< stackedArray.length; i++){
+    //     ctx.fillRect(newPlate.x, newPlate.y - stackedArray[i+1] * 50, stackedArray[i].w, stackedArray[i].h)
+    //     // detectCollision(newPlate, stackedArray[i])
+    // }
   
     // document.querySelector('body p').innerHTML = pointcounterBadTopping
 }
@@ -125,12 +152,49 @@ animate()
 
 // Collision Logics
 
-function detectCollision(thePlate, badTopping) {
+function detectBadToppingCollision(thePlate, badTopping) {
 if (thePlate.x < badTopping.x + badTopping.w &&
     thePlate.x + thePlate.w > badTopping.x &&
     thePlate.y < badTopping.y + badTopping.h &&
     thePlate.h + thePlate.y > badTopping.y) {
-    console.log('collision')
+    console.log('collision', badTopping)
+    badToppingsArr = badToppingsArr.filter(badItem => {
+        return badItem.id !== badTopping.id
+    })
+    // window.cancelAnimationFrame(int)
+    // stackedArray.push(badTopping)
+    // need to delete initial obj that collided with array
+    console.log(stackedArray)
+    // debugger
+    // window.location.reload()
+}
+}
+function detectPancakeCollision(thePlate, badTopping) {
+if (thePlate.x < badTopping.x + badTopping.w &&
+    thePlate.x + thePlate.w > badTopping.x &&
+    thePlate.y < badTopping.y + badTopping.h &&
+    thePlate.h + thePlate.y > badTopping.y) {
+    console.log('collision', badTopping)
+    // badToppingsArr = badToppingsArr.filter(badItem => {
+    //     return badItem.id !== badTopping.id
+    // })
+    // window.cancelAnimationFrame(int)
+    // stackedArray.push(badTopping)
+    // need to delete initial obj that collided with array
+    console.log(stackedArray)
+    // debugger
+    // window.location.reload()
+}
+}
+function detectBonusCollision(thePlate, badTopping) {
+if (thePlate.x < badTopping.x + badTopping.w &&
+    thePlate.x + thePlate.w > badTopping.x &&
+    thePlate.y < badTopping.y + badTopping.h &&
+    thePlate.h + thePlate.y > badTopping.y) {
+    console.log('collision', badTopping)
+    badToppingsArr = badToppingsArr.filter(badItem => {
+        return badItem.id !== badTopping.id
+    // })
     // window.cancelAnimationFrame(int)
     // stackedArray.push(badTopping)
     // need to delete initial obj that collided with array
